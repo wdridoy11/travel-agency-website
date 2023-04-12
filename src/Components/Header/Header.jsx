bbf0a647fb258507aca486fd363ad5b53c4eb403
Ridoy Sharif <developersridoy@gmail.com>
2023-04-12 10:41:12 +0600

    react font awesome installed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,8 +3,11 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-        <div>
+    <div className='bg-sky-300'>
+        <div className='container'>
+            <div>
+
+            </div>
             <nav>
                 <Link to="/">Home</Link>
                 <Link to="/place">Place</Link>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+            <h1 class="text-3xl font-bold underline">Home</h1>
+    </div>
   )
 }
 
