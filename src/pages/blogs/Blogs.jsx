e1f191f313ef2f56e498c9f8c6fce7d1cfab2849
Ridoy Sharif <developersridoy@gmail.com>
2023-07-04 12:33:31 +0600

    blog card content added

--- src/pages/blogs/Blogs.jsx
+++ src/pages/blogs/Blogs.jsx
@@ -13,7 +13,7 @@ console.log(blogs)
   return (
     <div className='pt-10 pb-20'>
         <div className='container mx-auto px-5'>
-            <h3 className="text-3xl font-semibold text-center mb-16">Our Blog</h3>
+            <h3 className="text-3xl font-semibold text-center mb-10">Our Blog</h3>
             <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-7'>
                 {blogs.map((blog)=><Blog key={blog.id} blog={blog}></Blog>)}
             </div>
